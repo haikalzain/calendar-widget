69a53cee30c3ca5c224e27db5eb099a00bf8811c
Haikal Zain <z5455019@ad.unsw.edu.au>
2023-07-18 17:49:19 +1000

    fix leap year

--- src/components/DatePicker/calendarData.js
+++ src/components/DatePicker/calendarData.js
@@ -51,8 +51,8 @@ export function getNameForWeek(week) {
 
 function isLeapYear(year) {
   if(year % 400 === 0) return 1
-  if(year % 100) return 0
-  if(year % 4) return 1
+  if(year % 100 === 0) return 0
+  if(year % 4 === 0) return 1
   return 0
 }
 
